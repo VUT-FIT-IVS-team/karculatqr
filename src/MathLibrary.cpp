1cd1920424ea9b8ce426d088c0011ce86e07e4c2
ProsteJarda <xotevr03@vutbr.cz>
2023-04-19 23:39:06 +0200

    feat: new func, not work by test

--- src/MathLibrary.cpp
+++ src/MathLibrary.cpp
@@ -60,19 +60,63 @@ void MathLibrary::subtract() {
 
 void MathLibrary::multiply() {
 	// TODO: Implement multiply
+	double value;
+	if (isPreviousResultSet) {
+		value = previousResult * currentValue;
+	}
+	else {
+		value = currentValue;
+	}
 }
 
 void MathLibrary::divide() {
 	// TODO: Implement divide
+	double value;
+	if (isPreviousResultSet) {
+		if (currentValue == 0) {
+			throw "Division by zero";
+		}else{
+			value = previousResult / currentValue;
+		}	
+	}
+	else {
+		value = currentValue;
+	}
 }
 
 void MathLibrary::calculateFactorial() {
 	// TODO: Implement calculateFactorial
+	double value;
+	if (currentValue < 0) {
+		throw "Factorial of negative number";
+	}
+	else if (currentValue == 0) {
+		value = 1;
+	}
+	else {
+		value = currentValue;
+		for (int i = currentValue - 1; i > 0; i--) {
+			value *= i;
+		}
+	}
 
 }
 
 void MathLibrary::calculatePower() {
 	// TODO: Implement calculatePower
+	double value;
+	if (isPreviousResultSet) {
+		for (int i = 0; i < currentValue; i++)
+		{
+			value *= previousResult;
+		}
+	}
+	else {
+		for (int i = 0; i < currentValue; i++)
+		{
+			value *= currentValue;
+		}
+	}
 }
 
 void MathLibrary::calculateRoot() {
