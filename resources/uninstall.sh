7d8211b3cc18d3eecca5bc5fe2eea6f83e8f9a8f
widowCutter <ondrej@pribyl.tech>
2023-04-26 04:50:07 +0200

    feat: finishing touches

--- resources/uninstall.sh
+++ resources/uninstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo rm /usr/bin/karculatqr
+sudo rm -f /usr/bin/karculatqr
 
-sudo rm /usr/share/applications/karculatqr.desktop
+sudo rm -f /usr/share/applications/karculatqr.desktop
+
+sudo rm -f /usr/bin/uninstall_karculatqr
