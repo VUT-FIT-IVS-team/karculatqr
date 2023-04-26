726dd46bbfcb65ffb15fe2afa6f63d33f48c2aaf
widowCutter <ondrej@pribyl.tech>
2023-04-26 03:38:25 +0200

    feat: makefile and help

--- resources/install.sh
+++ resources/install.sh
@@ -3,4 +3,7 @@ sudo apt-get -y install libqt5widgets5 libqt5gui5
 
 sudo cp ./karculatqr /usr/bin/karculatqr
 
+chmod a+x uninstall.sh
+sudo cp ./uninstall.sh /usr/bin/uninstall_karculatqr
+
 sudo cp karculatqr.desktop /usr/share/applications/karculatqr.desktop
---
+++ resources/installstddev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo cp ./stddev /usr/bin/stddev
